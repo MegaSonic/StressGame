dc746d335220fc38a67d3c1714f4487d58ee5fce
Luyao Tian <u1069212@ad.utah.edu>
2016-10-08 15:15:23 -0600

    Added temp BGM for main menu

---
+++ Assets/Core Game Assets/Scripts/BGMManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGMManager : MonoBehaviour {
+
+	public AudioClip menuBGM;
+
+	private AudioSource menuSource;
+
+	public AudioSource AddAudio(AudioClip clip, bool loop, bool playAwake, float vol)
+	{
+		AudioSource newAudio = gameObject.AddComponent<AudioSource> ();
+		newAudio.clip = clip;
+		newAudio.loop = loop;
+		newAudio.playOnAwake = playAwake;
+		newAudio.volume = vol;
+
+		return newAudio;
+	}
+
+	void Awake()
+	{
+		menuSource = AddAudio (menuBGM, true, false, 1.0f);
+		menuSource.Play ();
+	}
+}
