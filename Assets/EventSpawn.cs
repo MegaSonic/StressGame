eee28c04871dac8c7551a2c6eb610a567aace416
Adam Kay <kayad01@gettysburg.edu>
2016-10-11 01:30:33 -0600

    Added event spawn points

---
+++ Assets/EventSpawn.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventSpawn : MonoBehaviour {
+
+    public Transform despawnPoint;
+    public float timeBetweenSpawns;
+    public float eventMovementTime;
+
+
+    private GameObject eventPrefab;
+
+    private float spawnTimer;
+
+	// Use this for initialization
+	void Start () {
+        eventPrefab = Resources.Load("Event") as GameObject; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0f)
+        {
+            spawnTimer = timeBetweenSpawns;
+            GameObject eventObject = (GameObject) Instantiate(eventPrefab, transform.position, transform.rotation);
+            LeanTween.move(eventObject, despawnPoint.position, eventMovementTime);
+        }
+	}
+}
