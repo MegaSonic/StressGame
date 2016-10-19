41837237e002fa9dd709f00bac658bc96ac91d30
Luyao Tian <u1069212@ad.utah.edu>
2016-10-19 17:11:47 -0600

    Opening work in progress

---
+++ Assets/Core Game Assets/Scripts/Boundary.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Boundary : MonoBehaviour {
+
+	public Camera camera;
+	public Collider coll;
+	public float DelayTime;
+
+	private float xPos;
+	private float yPos;
+	private float reactTimer;
+
+
+	// Use this for initialization
+	void Start () {
+		GameObject go = GameObject.FindGameObjectWithTag ("startPlayer");
+		xPos = go.transform.position.x;
+		yPos = go.transform.position.y;
+		//Debug.Log (go.name);
+		reactTimer = DelayTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*
+		if (Input.GetButtonDown ("Fire1")) {
+			RaycastHit hit;
+			Ray ray = camera.ScreenPointToRay (Input.mousePosition);
+			if (coll.Raycast (ray, out hit, 100.0f)) {
+				float coordx = hit.point.x;
+				float coordy = hit.point.y;
+				float coordz = hit.point.z;
+				//Debug.Log ("Coordinate: (" + coordx + ", " + coordy + ", " + coordz + ")");
+			}
+		}
+		*/
+
+		Debug.Log ("Position: (" + xPos + ", " + yPos + ")");
+
+		//Debug.Log ("Time: " + reactTimer);
+		// position for QUIT
+		if (xPos > -5.8f && xPos < -2.25f && yPos > -1.6f && yPos < 1.1f) {
+			reactTimer -= Time.deltaTime;
+			if (reactTimer <= 0.0f)
+				Application.Quit ();
+		}
+
+		// position for BEGIN
+		if (xPos > 2.2f && xPos < 5.7f && yPos > -1.6f && yPos < 1.1f) {
+			reactTimer -= Time.deltaTime;
+			if (reactTimer <= 0.0f)
+				SceneManager.LoadScene ("MainGame");
+		}
+
+	}
+}
