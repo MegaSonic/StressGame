dd13857edf69914ee58da837c37d16af00476a55
MegaSonic <adamkay90@gmail.com>
2016-10-19 20:00:40 -0600

    Added Reset button to score screen

---
+++ Assets/ScoreButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ScoreButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ReturnToTitle()
+    {
+        SavedTimes save = GameObject.FindObjectOfType<SavedTimes>();
+        IconSelect icon = GameObject.FindObjectOfType<IconSelect>();
+        Destroy(save.gameObject);
+        Destroy(icon.gameObject);
+        SceneManager.LoadScene("Opening");
+
+    }
+}
